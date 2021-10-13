127eddc187c25b899cae587bf88a1aae47ef1e03
Charan_Reddy <89391662+CharanShelby@users.noreply.github.com>
2021-10-13 09:54:02 +0530

    add

---
+++ FirstConsole/FirstConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            what h = new what();
+            demo t = new demo();
+            t.todo();
+            h.why();
+            Console.WriteLine("Hello There !");
+            Console.ReadLine();
+        }
+        private static void staticmethod()
+        {
+            Console.WriteLine("okay...");
+
+        }
+    }
+    class demo {
+        public void todo()
+        {
+            Console.WriteLine("okay");
+        }
+        
+    }
+}
+    
+
